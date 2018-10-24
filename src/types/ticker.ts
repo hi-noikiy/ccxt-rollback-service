e4b9b848f5a61918bc99fe5c953bf5c44975212b
Stefan Hillebrand <hillebrand@neoskop.de>
2018-10-24 23:53:36 +0200

    chore

--- src/types/ticker.ts
+++ src/types/ticker.ts
@@ -1,5 +1,5 @@
 import { Ticker } from 'ccxt';
 
 export interface ITicker extends Ticker {
-  bidVolume?: number;
+  bidVolume?: number; // bidVolume is missing in ts-declaration due to its optional | undefined
 }
